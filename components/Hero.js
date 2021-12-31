172c37e75d452ba87763289f788714d2469ff8bc
Bilal <mansuribilal101@gmail.com>
2021-12-31 21:24:43 +0530

    hero section completed

--- components/Hero.js
+++ components/Hero.js
@@ -6,6 +6,12 @@ const Hero = () => {
                 <p> Google Developer Student Club </p>
                 <p>Medi-Caps Univeristy</p>
             </h1>
+            <div className={classes.hero_logo}>
+                <img src="./nakshatra.png" alt="Nakshatra Logo" />
+            </div>
+            <div className={classes.hero_year}>
+                <img src="./2022.png" alt="2022 Neon Text" />
+            </div>
         </section>
     )
 }
