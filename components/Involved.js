030daeb9fcc40c0933a00507eee41174d00b8b1c
Bilal <mansuribilal101@gmail.com>
2022-01-04 13:36:33 +0530

    involved section

---
+++ components/Involved.js
@@ -0,0 +1,27 @@
+import classes from './Involved.module.css';
+
+const Involved = () => {
+    return (
+        <section className={classes.involved}>
+            <h2>Get Involved</h2>
+            <div className={classes.involved_container}>
+                <p className={classes.container_subheading}>Sharing is caring, good things should reach to stars....involve your community and stuff</p>
+                <div className={classes.involved_actions}>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <div className={`${classes.btn_1} ${classes.action_button}`}>
+                            <img src="/partner.svg" alt="Partner" />
+                            <p>Become community partner</p>
+                        </div>
+                    </a>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <div className={`${classes.btn_2} ${classes.action_button}`}>
+                            <img src="/community.svg" alt="Community" />
+                            <p>Become community manager</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Involved;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Pages from "../components/Pages"
 import Events from "../components/Events"
 import Speakers from "../components/Speakers"
 import Timeline from "../components/Timeline"
+import Involved from "../components/Involved"
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Events />
         <Speakers />
         <Timeline />
+        <Involved />
       </main>
     </>
   )
